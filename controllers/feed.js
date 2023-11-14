f98bb77d7249654ea0e809a88e92876d692162ab
Radu Baltag <baltag.radu@gmail.com>
2023-11-14 13:56:57 +0100

    updated controllers to work w FE

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,16 +1,32 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/car.jpg',
+        creator: {
+          name: 'Radu',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  console.log(title, content);
   //create in DB
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: Math.floor(Math.random() * 1000), title, content },
+    post: {
+      _id: Math.floor(Math.random() * 1000),
+      title,
+      content,
+      creator: { name: 'Radu' },
+      createdAt: new Date(),
+    },
   });
 };
